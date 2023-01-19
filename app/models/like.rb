16742afedac0ec2a02c41347c6fb9301d6c2fe4f
Kelvin-Ben <[kevoben14@gmail.com]>
2023-01-19 23:21:45 +0300

    Update model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ApplicationRecord
-  belongs_to :user, index: true, foreign_key: true
-  belongs_to :post, index: true, foreign_key: true
+  belongs_to :auhtor, class_name: 'User', index: true, foreign_key: :author_id
+  belongs_to :post, index: true, foreign_key: :post_id
 end
