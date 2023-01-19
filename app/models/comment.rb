83fdb500424c85193e10fe86e31844f9f3939d48
Kelvin-Ben <[kevoben14@gmail.com]>
2023-01-19 23:21:08 +0300

    Create at least 6 posts comments for one of the posts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-  belongs_to :user, index: true, foreign_key: true
-  belongs_to :post, index: true, foreign_key: true
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :post, foreign_key: :post_id
 end
