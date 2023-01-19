0cb4dd1d82eca44696ef1cf0e2c0faa15d44074b
Kelvin-Ben <[kevoben14@gmail.com]>
2023-01-19 23:22:21 +0300

    Create at least 4 posts written by one of the users

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,22 @@
 class Post < ApplicationRecord
-  belongs_to :user, index: true, foreign_key: true
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :likes
-  has_many :Comments
+  has_many :comments, foreign_key: :post_id
 end
+
+# first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+# second_post = Post.create(author: second_user, title: 'Hey', text: 'This is my second post')
+# third_post = Post.create(author: first_user, title: 'Hello world', text: 'This is my first page')
+# fourth_post = Post.create(author: second_user, title: 'I am a developer', text: 'I love what I do')
+# fifth_post = Post.create(author: first_user, title: 'I like this post', text: 'It was amazing to read it')
+# sixth_post = Post.create(author: second_user, title: 'How amazing?', text: 'How do I become a writer too?')
+# seventh_post = Post.create(author: first_user, title: 'The reading is Great', text: 'I enjoyed the article')
+# eighth_post = Post.create(author: second_user, title: 'How cool is it?', text: 'One of amzing reading in my life!')
+
+
+        # Comment.create(post: third_post, author: first_user, text: 'Hi Tom!')
+        # Comment.create(post: third_post, author: first_user,  text: 'Hello Ken')
+        # Comment.create(post: third_post, author: second_user, text: 'How are you?')
+        # Comment.create(post: third_post, author: first_user,  text: 'Which article to read again?')
+        # Comment.create(post: third_post, author: second_user, text: 'I am happy for you')
+        # Comment.create(post: third_post, author: first_user,  text: 'Nice to third_post
