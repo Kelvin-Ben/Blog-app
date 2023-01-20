10f6ecd361b3e475669461dc2f587af6f1de41f1
Kelvin-Ben <[kevoben14@gmail.com]>
2023-01-20 09:39:56 +0300

    Add validations to validate  Name and PostsCounter

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   has_many :comments, through: :posts
   has_many :likes, through: :posts
 
+  validates_presence_of :name, message: "Name Cannot be Blank!"
+  validates_numericality_of :posts_count, greater_than_or_equal_to: 0, message: 
+    "posts counter must be an integer greater_than_or_equal_to zero"
+
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
