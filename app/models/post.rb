0b6dfeabcdf65df7e4824c05f04ceb362e3aed0f
Kelvin-Ben <[kevoben14@gmail.com]>
2023-01-20 07:42:29 +0300

    Remove commented codebase

--- app/models/post.rb
+++ app/models/post.rb
@@ -16,18 +16,3 @@ class Post < ApplicationRecord
   end
 end
 
-# first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
-# second_post = Post.create(author: second_user, title: 'Hey', text: 'This is my second post')
-# third_post = Post.create(author: first_user, title: 'Hello world', text: 'This is my first page')
-# fourth_post = Post.create(author: second_user, title: 'I am a developer', text: 'I love what I do')
-# fifth_post = Post.create(author: first_user, title: 'I like this post', text: 'It was amazing to read it')
-# sixth_post = Post.create(author: second_user, title: 'How amazing?', text: 'How do I become a writer too?')
-# seventh_post = Post.create(author: first_user, title: 'The reading is Great', text: 'I enjoyed the article')
-# eighth_post = Post.create(author: second_user, title: 'How cool is it?', text: 'One of amzing reading in my life!')
-
-# Comment.create(post: third_post, author: first_user, text: 'Hi Tom!')
-# Comment.create(post: third_post, author: first_user,  text: 'Hello Ken')
-# Comment.create(post: third_post, author: second_user, text: 'How are you?')
-# Comment.create(post: third_post, author: first_user,  text: 'Which article to read again?')
-# Comment.create(post: third_post, author: second_user, text: 'I am happy for you')
-# Comment.create(post: third_post, author: first_user,  text: 'Nice to third_post
