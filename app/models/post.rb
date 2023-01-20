983e2fbf0cc33e71aaecd4f9cdd6c95844b7e931
Kelvin-Ben <[kevoben14@gmail.com]>
2023-01-20 09:51:39 +0300

    Add validations for comments and likes counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments, foreign_key: :post_id
 
+  
+  validates_numericality_of :comments_count, greater_than_or_equal_to: 0, message: 
+    "Comments counter must be an integer greater than or eaqual to zero"
+  validates_numericality_of :likes_count, greater_than_or_equal_to: 0, message:
+    "Likes counter must be an integer greater than or equal to zero"
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
