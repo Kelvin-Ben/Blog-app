79424764e3664f6c1377701b6e2b66d72537ee69
Kelvin-Ben <[kevoben14@gmail.com]>
2023-01-21 10:17:59 +0300

    Update model associations

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ApplicationRecord
-  belongs_to :auhtor, class_name: 'User', index: true, foreign_key: :author_id
-  belongs_to :post, index: true, foreign_key: :post_id
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :post, foreign_key: :post_id
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,20 +4,21 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: :post_id
 
   validates :title, presence: true
-  validates :title, length: { maximum: 250, too_long: "Name must not exceed 250 characters"}
-  validates_numericality_of :comments_count, greater_than_or_equal_to: 0, message: 
-    "Comments counter must be an integer greater than or eaqual to zero"
-  validates_numericality_of :likes_count, greater_than_or_equal_to: 0, message:
-    "Likes counter must be an integer greater than or equal to zero"
+  validates :title, length: { maximum: 250, too_long: 'Name must not exceed 250 characters' }
+  validates_numericality_of :comments_counter, greater_than_or_equal_to: 0, message:
+    'Comments counter must be an integer greater than or eaqual to zero'
+  validates_numericality_of :likes_counter, greater_than_or_equal_to: 0, message:
+    'Likes counter must be an integer greater than or equal to zero'
+
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
 
   def update_comments_counter
-    update(comments_count: comments.count)
+    update(comments_counter: comments.count)
   end
 
   def update_likes_counter
-    update(likes_count: likes.count)
+    update(likes_counter: likes.count)
   end
 end
