08414dd3d8c58d9b5e24a098eb159e09170d5f60
Kelvin-Ben <[kevoben14@gmail.com]>
2023-01-24 18:54:27 +0300

    Modified the users and posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  def index; end
-
-  def show; end
+  def index
+  end
+  def show
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  def index; end
-
-  def show; end
+  def index
+  end
+  def show
+  end
 end
