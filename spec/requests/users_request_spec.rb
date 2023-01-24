10c664757af9eb1c732c6633c739660d78bd5c4b
Kelvin-Ben <[kevoben14@gmail.com]>
2023-01-24 22:56:44 +0300

    Add unit testing to the users

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
+RSpec.describe 'Users', type: :request do
+  describe 'GET /users/:id' do
+    it 'returns a 200 status code' do
+      get user_path(:user)
+      expect(response).to have_http_status(200)
+    end
+  end
 
+  describe 'GET /users/:id' do
+    it 'renders the show template' do
+      get user_path(:user)
+      expect(response).to render_template(:show)
+    end
+  end
+
+  describe 'GET /users/:id' do
+    it 'If the response body includes correct placeholder text.' do
+      get user_path(:user)
+      expect(response.body).to include('Welcome to the users page')
+    end
+  end
 end
