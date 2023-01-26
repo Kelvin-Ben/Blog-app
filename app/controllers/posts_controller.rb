f22572f58122553bec8daf53006c6177bd49cec0
Kelvin-Ben <[kevoben14@gmail.com]>
2023-01-26 10:33:07 +0300

    Update the index method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    # @posts = Post.all
+    @users = User.find(params[:user_id])
   end
 
   def show
-    @posts = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 end
