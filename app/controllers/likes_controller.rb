fef8ba8433486520ab51fa4423fed49169412245
Kelvin-Ben <[kevoben14@gmail.com]>
2023-01-27 09:51:09 +0300

    Add a method responsible for adding new like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,15 @@
 class LikesController < ApplicationController
+  def create
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    @like = Like.new
+    @like.auhtor = current_user
+    @like.post = @post
+
+    if @like.save
+      redirect_to user_post_path(@user, @post)
+    else
+      render 'posts/show'
+    end
+  end
 end
