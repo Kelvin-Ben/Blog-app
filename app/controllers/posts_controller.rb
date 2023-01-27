dd8e4af6cea46afb209a32acc4a0340eb9350dc4
Kelvin-Ben <[kevoben14@gmail.com]>
2023-01-27 10:02:04 +0300

    Created a method responsible for creating a new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,32 @@
 class PostsController < ApplicationController
   def index
-    # @posts = Post.all
     @users = User.find(params[:user_id])
   end
 
   def show
+    @post = Post.find(params[:user_id])
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+    if @post.save
+      flash[:notice] = 'Post saved successfully'
+      redirect_to user_posts_path(current_user)
+    else
+      flash.now[:error] = 'Post could not be created!!'
+      render :new
+    end
+  end
+
+    private
+# private method which will be used to filter the parameters that are passed to the create method.
+    def post_params
+      params.required(:post).permit(:title, :text)
+    end
 end
