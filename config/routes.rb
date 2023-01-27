bd445ff9ea82cdca08a1ca69052fa1f412f1e4f7
Kelvin-Ben <[kevoben14@gmail.com]>
2023-01-27 16:27:29 +0300

    Added routes of the controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:create]
+      resources :likes, only: [:create]
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-
   root 'users#index'
-
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
-  end
+  # get '/posts/new', to: 'posts#new'
+  # post '/posts', to: 'posts#create'
 end
