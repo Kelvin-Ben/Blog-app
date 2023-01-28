8b1cf49e409c9a4a478047757029d63030a0280e
Kelvin-Ben <[kevoben14@gmail.com]>
2023-01-28 14:01:39 +0300

    Remove commented code

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root 'users#index'
+  
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:create]
       resources :likes, only: [:create]
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root 'users#index'
-  # get '/posts/new', to: 'posts#new'
-  # post '/posts', to: 'posts#create'
 end
