5d54cd23b3345c49cb1626e88c5c6df73325363a
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-01-31 11:45:24 -0300

    Fix view issue

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,8 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:user_id])
     @post = Post.find(params[:id])
+    @comments = Comment.where(post_id: params[:id])
   end
 
   def new
