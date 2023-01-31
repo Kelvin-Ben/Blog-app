b8e230e487b4d89bef2097d26f6b4f3fe2d298ab
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-01-31 12:07:44 -0300

    Replace all with each

--- spec/integration/user_index_spec.rb
+++ spec/integration/user_index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Home page', type: :system do
-  before :all do
+  before :each do
     @tom = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
     @lilly = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BHHuVvo', bio: 'Teacher from Poland')
     @linktester = User.create(name: 'Linktester', photo: 'https://unsplash.com/photos/F_-0BHHuVvo', bio: 'Teacher from Poland')
@@ -9,7 +9,7 @@ RSpec.describe 'Home page', type: :system do
     @post2 = Post.create(title: 'post2', text: 'text2', author_id: @tom.id)
     @post3 = Post.create(title: 'post3', text: 'text1', author_id: @tom.id)
     @post4 = Post.create(title: 'post4', text: 'text2', author_id: @tom.id)
-    @post3 = Post.create(title: 'post3', text: 'text3', author_id: @lilly.id)
+    @post5 = Post.create(title: 'post5', text: 'text3', author_id: @lilly.id)
   end
   describe 'index page' do
     it 'shows the right username' do
