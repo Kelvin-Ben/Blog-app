09d0e3ee48cdfe3c00b6dfbb954feb718a3e0614
Kelvin-Ben <[kevoben14@gmail.com]>
2023-02-01 20:15:12 +0300

    Remove the validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: :author_id
   has_many :comments, through: :posts
   has_many :likes, through: :posts
-
-  validates :name, presence: true
-  validates_numericality_of :posts_counter, greater_than_or_equal_to: 0, message:
-    'posts counter must be an integer greater_than_or_equal_to zero'
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
