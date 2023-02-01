21092db20f948a4e5f5f4dc749cd71c017d0c618
Kelvin-Ben <[kevoben14@gmail.com]>
2023-02-01 08:22:32 +0300

    Remove currect user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
-  # before_action :current_user
-  # @current_user ||= User.find_by(id: session[:user_id])
-  def current_user
-    User.first
+  protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
   end
 end
