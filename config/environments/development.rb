d6b7d6326f013151ae17ccfdfe1d246661374977
Kelvin-Ben <[kevoben14@gmail.com]>
2023-02-01 20:18:22 +0300

    Added action mailers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,7 +61,9 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 
