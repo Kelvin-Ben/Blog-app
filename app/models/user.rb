ac294c988ea38d70b33f71927a68d1790e7d083a
Kelvin-Ben <[kevoben14@gmail.com]>
2023-02-02 12:56:17 +0300

    Added a few validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: :author_id
-  has_many :comments, through: :posts
-  has_many :likes, through: :posts
+  has_many :comments, foreign_key: :author_id
+  has_many :likes, foreign_key: :author_id
+
+  validates :name, presence: true, allow_blank: false
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_posts
-    posts.order(created_at: :desc).limit(3)
+    posts.order('created_at DESC').first(3)
   end
 end
