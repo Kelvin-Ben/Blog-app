e1a216f7cc736abadeaba0db27ee4215ce7e1f6c
Kelvin-Ben <[kevoben14@gmail.com]>
2023-02-02 12:53:00 +0300

    Added permision for several parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # protect_from_forgery prepend: true
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
-  before_action :update_allowed_parameters, if: :devise_controller?
+  private
+
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
 
   protected
 
-  def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
   end
 end
